59c30a6659b78c740cb4897f8f67fb23f1eed0bc
Indika Wijesooriya <indikman@gmail.com>
2024-02-03 08:51:12 -0700

    Grid System basic

--- Assets/Match3/Scripts/GridSystem2D.cs
+++ Assets/Match3/Scripts/GridSystem2D.cs
@@ -15,11 +15,34 @@ namespace IndiMatchThree
         {
             public abstract Vector3 GridToWorld(int x, int y, float cellSize, Vector3 origin);
 
-            public abstract Vector3 GridWorldCenter(int x, int y, float cellSize, Vector3 origin);
+            public abstract Vector3 GridToWorldCenter(int x, int y, float cellSize, Vector3 origin);
 
             public abstract Vector2Int WorldToGrid(Vector3 worldPosition, float cellSize, Vector3 origin);
 
             public abstract Vector3 Forward { get; }
+        }
+
+        public class VerticalConverter : CoordinateConverter
+        {
+            public override Vector3 Forward => Vector3.forward;
+
+            public override Vector3 GridToWorld(int x, int y, float cellSize, Vector3 origin)
+            {
+                return new Vector3(x, y, 0) * cellSize + origin;
+            }
+
+            public override Vector3 GridToWorldCenter(int x, int y, float cellSize, Vector3 origin)
+            {
+                return new Vector3(x*cellSize + cellSize * 0.5f, y * cellSize + cellSize * 0.5f, 0) + origin;
+            }
+
+            public override Vector2Int WorldToGrid(Vector3 worldPosition, float cellSize, Vector3 origin)
+            {
+                var gridPosition = (worldPosition - origin) / cellSize;
+                var x = Mathf.FloorToInt(gridPosition.x);
+                var y = Mathf.FloorToInt(gridPosition.y);
+                return new Vector2Int(x, y);
+            }
         }
     }
 }
