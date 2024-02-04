e5b3331a234720d72f3379e90c09e110246bc048
Indika Wijesooriya <indikman@gmail.com>
2024-02-04 11:17:25 -0700

    Assembly ref added

--- Assets/Match3/Scripts/Gem.cs
+++ Assets/Match3/Scripts/Gem.cs
@@ -1,7 +1,18 @@
-﻿namespace IndiMatchThree
-{
-    public class Gem
-    {
+﻿using UnityEngine;
 
+namespace IndiMatchThree
+{
+    [RequireComponent(typeof(SpriteRenderer))]
+    public class Gem:MonoBehaviour
+    {
+        public GemType type;
+
+        public void SetType(GemType type)
+        {
+            this.type = type;
+            GetComponent<SpriteRenderer>().sprite = type.sprite;
+        }
+
+        public GemType GetType() => type;
     }
 }
---
+++ Assets/Match3/Scripts/GemType.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace IndiMatchThree
+{
+    [CreateAssetMenu(fileName ="GemType", menuName ="Match3/GemType")]
+    public class GemType : ScriptableObject
+    {
+        public Sprite sprite;
+    }
+}
